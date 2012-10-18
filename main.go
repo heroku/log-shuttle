d80a52458ba318b0f6d8a2509c311cecd20ec18c
♠ ace hacker <ryan@heroku.com>
2012-10-17 22:59:34 -0700

    prepare messages for logplex by using rfc5424 format

--- main.go
+++ main.go
@@ -11,10 +11,23 @@ import (
 	"encoding/json"
 )
 
+func prepare(batch []string) string {
+	result := ""
+	length := 0
+	for _, msg := range batch {
+		t := time.Now().UTC().Format(time.RFC3339 + " ")
+		//<prival>version time app-name procid msgid msg \n
+		line := "<0>1 " + t + "1234 " + "5678 " + "- " + msg + " \n"
+		result += line
+		length += len(line)
+	}
+	return fmt.Sprintf("%d", length) + " " + result
+}
+
 func outlet(batches <-chan []string) {
 	for batch := range batches {
 		url := "http://httpbin.org/post"
-		b, _ := json.Marshal(batch)
+		b, _ := json.Marshal(prepare(batch))
 		resp, err := http.Post(url, "application/json", bytes.NewBuffer(b))
 		defer resp.Body.Close()
 		if err != nil {
