3619165ea6928eb288da72c6eada4e2d2d318c3a
♠ ace hacker <ryan@heroku.com>
2012-12-18 12:45:15 -0800

    can't deref in var block

--- main.go
+++ main.go
@@ -24,21 +24,21 @@ var (
 
 // Flags
 var (
-	frontBuff            = *flag.Int("front-buff", 0, "Number of messages to buffer in log-shuttle's input chanel.")
-	batchSize            = *flag.Int("batch-size", 50, "Number of messages to pack into a logplex http request.")
-	wait                 = *flag.Int("wait", 500, "Number of ms to flush messages to logplex")
-	workerCount          = *flag.Int("workers", 1, "Number of concurrent outlet workers (and HTTP connections)")
-	socket               = *flag.String("socket", "", "Location of UNIX domain socket.")
-	logplexToken         = *flag.String("logplex-token", "abc123", "Secret logplex token.")
-	procid               = *flag.String("procid", "", "The procid for the syslog payload")
-	skipHeaders          = *flag.Bool("skip-headers", false, "Skip the prepending of rfc5424 headers.")
-	skipCertVerification = *flag.Bool("skip-cert-verification", false, "Disable SSL cert validation.")
+	frontBuff            = flag.Int("front-buff", 0, "Number of messages to buffer in log-shuttle's input chanel.")
+	batchSize            = flag.Int("batch-size", 50, "Number of messages to pack into a logplex http request.")
+	wait                 = flag.Int("wait", 500, "Number of ms to flush messages to logplex")
+	workerCount          = flag.Int("workers", 1, "Number of concurrent outlet workers (and HTTP connections)")
+	socket               = flag.String("socket", "", "Location of UNIX domain socket.")
+	logplexToken         = flag.String("logplex-token", "abc123", "Secret logplex token.")
+	procid               = flag.String("procid", "", "The procid for the syslog payload")
+	skipHeaders          = flag.Bool("skip-headers", false, "Skip the prepending of rfc5424 headers.")
+	skipCertVerification = flag.Bool("skip-cert-verification", false, "Disable SSL cert validation.")
 )
 
 func init() {
 	flag.Parse()
-	if workerCount < 1 {
-		workerCount = 1 // workerCount needs to be >= 1
+	if *workerCount < 1 {
+		*workerCount = 1 // workerCount needs to be >= 1
 	}
 }
 
@@ -57,10 +57,10 @@ func init() {
 	// If the username and password weren't part of the URL, use the
 	// logplex-token as the password
 	if logplexUrl.User == nil {
-		logplexUrl.User = url.UserPassword("token", logplexToken)
+		logplexUrl.User = url.UserPassword("token", *logplexToken)
 	}
 	if logplexUrl.Scheme == "https" {
-		tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: skipCertVerification}}
+		tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: *skipCertVerification}}
 		http.DefaultTransport = tr
 	}
 }
@@ -171,19 +171,19 @@ func main() {
 	var drops uint64 = 0 //count the number of droped lines
 	var reads uint64 = 0 //count the number of read lines
 	batches := make(chan []string)
-	lines := make(chan string, frontBuff)
+	lines := make(chan string, *frontBuff)
 
 	go report(lines, batches, &drops, &reads)
-	go handle(lines, batches, batchSize, wait)
-	for i := 0; i < workerCount; i++ {
-		go outlet(batches, logplexToken, logplexUrl.String(), procid, skipHeaders)
+	go handle(lines, batches, *batchSize, *wait)
+	for i := 0; i < *workerCount; i++ {
+		go outlet(batches, *logplexToken, logplexUrl.String(), *procid, *skipHeaders)
 	}
 
-	if len(socket) == 0 {
+	if len(*socket) == 0 {
 		read(os.Stdin, lines, &drops, &reads)
 		reqInFlight.Wait()
 	} else {
-		l, err := net.Listen("unix", socket)
+		l, err := net.Listen("unix", *socket)
 		if err != nil {
 			log.Fatal(err)
 		}
