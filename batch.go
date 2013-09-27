242b7e77b39415ad3a280778804ab317e796b4e3
Edward Muller <edwardam@interlix.com>
2013-09-27 11:07:53 -0700

    Make anonymous to remove proxy functions

--- batch.go
+++ batch.go
@@ -9,7 +9,7 @@ import (
 // keeps track of line's Write()n to the buffer
 type Batch struct {
 	lineCount int
-	buffer    bytes.Buffer
+	bytes.Buffer
 }
 
 func (b *Batch) LineCount() int {
@@ -18,19 +18,15 @@ func (b *Batch) LineCount() int {
 
 // Write a line to the batch, increment it's line counter
 func (b *Batch) Write(line *string) {
-	fmt.Fprintf(&b.buffer, "%d %s", len(*line), *line)
+	fmt.Fprintf(&b.Buffer, "%d %s", len(*line), *line)
 	b.lineCount++
 }
 
 // Zero the line count and reset the internal buffer
 func (b *Batch) Reset() {
 	b.lineCount = 0
-	b.buffer.Reset()
+	b.Buffer.Reset()
 }
-
-// Proxy to buffer
-func (b *Batch) Len() int                         { return b.buffer.Len() }
-func (b *Batch) Read(p []byte) (n int, err error) { return b.buffer.Read(p) }
 
 // NoOpCloser
 func (b *Batch) Close() error { return nil }
