d86a26f4edb3245729ce09cdf11010b11004a9c1
Edward Muller <edwardam@interlix.com>
2013-10-22 12:39:12 -0700

    Rework the batching and related parts... STILL A WIP
    - Trade buffers for non-blocking
    - Introduction of a batch manager.
    - Simplify the process of scaling batchers and outlets
    - Possibly better distribution via the outlets (one http.Client per outlet)
    - expire old batches to handle bursts
    - move line formatting out of the reader(s) and into the batcher(s)
    - Combine stats into a struct so there is just less to pass around.
    
    There is now a more simple interface between batchers and outlets, which
    is a trio of channels: inBatches; outBatches; returnBatches.
    
    inBatches is used by the batchers to get a new batch from the batch
    manager.
    outBatches is used by the batchers to send "complete" batches to
    outlets.
    returnBatches is used by outlets to return batches to the batch manager.
    
    The batch manager currently implements a non blocking (mostly) aged LIFO
    list of buckets and will expire buckets that have been queued for > 1
    minute. This should help handle bursts as unused buckets will age out
    over time.
    
    We could implement other batch managers using the give/return channel
    set if this behaviour is not up to snuff.

--- batch.go
+++ batch.go
@@ -5,20 +5,46 @@ import (
 	"fmt"
 )
 
+var syslogLineLayout = "<%s>%s %s %s %s %s %s %s"
+
 // A buffer suitable for posting with a http client
 // keeps track of line's Write()n to the buffer
 type Batch struct {
 	lineCount int
+	config    *ShuttleConfig
 	bytes.Buffer
+}
+
+func NewBatch(config *ShuttleConfig) (batch *Batch) {
+	return &Batch{config: config}
 }
 
 func (b *Batch) LineCount() int {
 	return b.lineCount
 }
 
+func (b *Batch) FormatLine(logLine *LogLine) (int, *string) {
+	if b.config.SkipHeaders {
+		return len(logLine.line), &logLine.line
+	} else {
+		formattedLine := fmt.Sprintf(
+			syslogLineLayout,
+			b.config.Prival,
+			b.config.Version,
+			logLine.when.Format("2006-01-02T15:04:05.000000+00:00"),
+			b.config.Hostname,
+			b.config.Appname,
+			b.config.Procid,
+			b.config.Msgid,
+			logLine.line)
+		return len(formattedLine), &formattedLine
+	}
+}
+
 // Write a line to the batch, increment it's line counter
-func (b *Batch) Write(line *string) {
-	fmt.Fprintf(&b.Buffer, "%d %s", len(*line), *line)
+func (b *Batch) Write(logLine *LogLine) {
+	fll, fl := b.FormatLine(logLine)
+	fmt.Fprintf(&b.Buffer, "%d %s", fll, fl)
 	b.lineCount++
 }
 
---
+++ batch_manager.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"container/list"
+	"time"
+)
+
+type queued struct {
+	when  time.Time
+	batch *Batch
+}
+
+func BatchManager(getBatches, returnBatches chan *Batch, config *ShuttleConfig) {
+	q := new(list.List)
+	ticker := time.Tick(time.Minute)
+
+	for {
+		if q.Len() == 0 {
+			q.PushFront(&queued{when: time.Now(), batch: NewBatch(config)})
+		}
+
+		e := q.Front()
+
+		select {
+		case batch := <-returnBatches:
+			//I've been given a batch back, queue it
+			batch.Reset()
+			q.PushFront(queued{when: time.Now(), batch: batch})
+
+		case getBatches <- e.Value.(queued).batch:
+			//I've sent the current batch out, remove it
+			q.Remove(e)
+
+		case <-ticker:
+			//Periodically go through the queued batches and throw
+			//out ones that have been queued for too long in an effort
+			//to expire old batches that were created because of bursts
+			for e := q.Front(); e != nil; e = e.Next() {
+				if time.Since(e.Value.(queued).when) > time.Minute {
+					q.Remove(e)
+					e.Value = nil
+				}
+			}
+		}
+	}
+}
+
+func NewBatchManager(config ShuttleConfig) (getBatches, returnBatches chan *Batch) {
+	getBatches = make(chan *Batch)
+	returnBatches = make(chan *Batch)
+
+	go BatchManager(getBatches, returnBatches, &config)
+
+	return
+}
