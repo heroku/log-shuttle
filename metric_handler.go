10d69a1d75b65b6037cc392cd2fd2834fbd1b5f2
Edward Muller <edwardam@interlix.com>
2013-11-22 18:03:54 -0600

    WIP

--- metric_handler.go
+++ metric_handler.go
@@ -34,14 +34,41 @@ func (mo *MetricOutputter) Start() {
 			continue
 		}
 		if mh.data["ns"] == "writer" && mh.data["at"] == "finish" {
-			v, err := strconv.ParseFloat(mh.data["elapsed"], 64)
+			err = ddClient.Count("files.uploaded", 1, nil, 1)
+			if err != nil {
+				log.Println("Error sending data (files.uploaded): ", err)
+				continue
+			}
+			ev, err := strconv.ParseFloat(mh.data["elapsed"], 64)
 			if err != nil {
 				log.Println("Error parsing float ("+mh.data["elapsed"]+"): ", err)
 				continue
 			}
-			err = ddClient.Histogram("s3.write", v, nil, 1)
+			err = ddClient.Histogram("s3.write", ev, nil, 1)
 			if err != nil {
-				log.Println("Error sending data: ", err)
+				log.Println("Error sending data (s3.write): ", err)
+				continue
+			}
+
+			cv, err := strconv.ParseInt(mh.data["count"], 10, 64)
+			if err != nil {
+				log.Println("Error parsing int("+mh.data["count"]+"): ", err)
+				continue
+			}
+			err = ddClient.Count("lines.uploaded", cv, nil, 1)
+			if err != nil {
+				log.Println("Error sending data (lines.uploaded): ", err)
+				continue
+			}
+
+			csv, err := strconv.ParseInt(mh.data["compressed_size"], 10, 64)
+			if err != nil {
+				log.Println("Error parsing int("+mh.data["compressed_size"]+"): ", err)
+				continue
+			}
+			err = ddClient.Count("bytes.uploaded", csv, nil, 1)
+			if err != nil {
+				log.Println("Error sending data (bytes.uploaded): ", err)
 				continue
 			}
 
