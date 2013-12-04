fb91306119337bb3f68ada6d033a17f9114b625b
Edward Muller <edwardam@interlix.com>
2013-12-04 10:42:36 -0800

    simplify

--- program_stats.go
+++ program_stats.go
@@ -14,12 +14,11 @@ const (
 
 func NewProgramStats(bi chan LogLine, oi chan *Batch) *ProgramStats {
 	return &ProgramStats{
-		dropsMutex:   new(sync.Mutex),
-		lostMutex:    new(sync.Mutex),
 		batchInput:   bi,
 		outletInput:  oi,
 		StatsChannel: make(chan NamedValue, STATS_CHANNEL_BUFFER_SIZE),
 		stats:        make(map[string]*quantile.Stream),
+		Mutex:        new(sync.Mutex),
 	}
 }
 
@@ -65,12 +64,12 @@ func (stats *ProgramStats) StartPeriodicReporter(config ShuttleConfig) {
 
 func logStats(source string, thing string, logger *log.Logger, stats *quantile.Stream) {
 	if stats.Count() > 0 {
-		logger.Printf("source=%s sample#log-shuttle.%s.time.p50=%fs sample#log-shuttle.%[2]s.time.p95=%[4]fs sample#log-shuttle.%[2]s.time.p99=%[5]fs\n",
+		logger.Printf("source=%s count#log-shuttle.%s.count=%d sample#log-shuttle.%s.p50=%f sample#log-shuttle.%s.p95=%f sample#log-shuttle.%s.p99=%f\n",
 			source,
-			thing,
-			stats.Query(0.50),
-			stats.Query(0.95),
-			stats.Query(0.99),
+			thing, stats.Count(),
+			thing, stats.Query(0.50),
+			thing, stats.Query(0.95),
+			thing, stats.Query(0.99),
 		)
 		stats.Reset()
 	}
@@ -97,32 +96,31 @@ type ProgramStats struct {
 	outletInput       chan *Batch
 	stats             map[string]*quantile.Stream
 	StatsChannel      chan NamedValue
-	dropsMutex        *sync.Mutex
-	lostMutex         *sync.Mutex
+	*sync.Mutex
 }
 
 func (ps *ProgramStats) IncrementDrops(i uint64) uint64 {
-	ps.dropsMutex.Lock()
-	defer ps.dropsMutex.Unlock()
+	ps.Lock()
+	defer ps.Unlock()
 	ps.CurrentDrops.Add(i)
 	return ps.AllTimeDrops.Add(i)
 }
 
 func (ps *ProgramStats) ReadAndResetDrops() uint64 {
-	ps.dropsMutex.Lock()
-	defer ps.dropsMutex.Unlock()
+	ps.Lock()
+	defer ps.Unlock()
 	return ps.CurrentDrops.ReadAndReset()
 }
 
 func (ps *ProgramStats) IncrementLost(i uint64) uint64 {
-	ps.lostMutex.Lock()
-	defer ps.lostMutex.Unlock()
+	ps.Lock()
+	defer ps.Unlock()
 	ps.CurrentLost.Add(i)
 	return ps.AllTimeLost.Add(i)
 }
 
 func (ps *ProgramStats) ReadAndResetLost() uint64 {
-	ps.lostMutex.Lock()
-	defer ps.lostMutex.Unlock()
+	ps.Lock()
+	defer ps.Unlock()
 	return ps.CurrentLost.ReadAndReset()
 }
