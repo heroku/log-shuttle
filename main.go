589be772e4791723bae62b3145cd78c77dc52be7
Edward Muller <edwardam@interlix.com>
2014-04-16 13:13:38 -0700

    bump version for testing

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var (
 )
 
 const (
-	VERSION = "0.8.1"
+	VERSION = "0.9.0"
 )
 
 func MakeBasicBits(config ShuttleConfig) (reader *Reader, deliverableBatches chan *Batch, programStats *ProgramStats, bWaiter, oWaiter *sync.WaitGroup) {
