9ee8cce5b9eeac1394d512ed9b036d0dfe4c4731
trestletech <jeff.allen@trestletechnology.net>
2014-07-16 08:22:33 -0500

    Refactoring into 'shuttle' package.

--- shuttle/batch.go
+++ shuttle/batch.go
@@ -1,4 +1,4 @@
-package main
+package shuttle
 
 import "github.com/nu7hatch/gouuid"
 
---
+++ shuttle/loggers.go
@@ -0,0 +1,16 @@
+
+package shuttle
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	Logger    = log.New(os.Stdout, "log-shuttle: ", log.LstdFlags)
+	ErrLogger = log.New(os.Stderr, "log-shuttle: ", log.LstdFlags)
+)
+
+const (
+	VERSION = "0.9.6"
+)
