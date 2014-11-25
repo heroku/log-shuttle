f1009686b24c6281f2b6fb0a08dc97a8ec78aee9
Edward Muller <edwardam@interlix.com>
2014-11-24 20:07:46 -0800

    golint shuttle/logline.go

--- shuttle/logline.go
+++ shuttle/logline.go
@@ -2,11 +2,13 @@ package shuttle
 
 import "time"
 
+// LogLine holds the new line terminated log messages and when shuttle received them.
 type LogLine struct {
 	line []byte
 	when time.Time
 }
 
+// Length returns the length of the raw byte of the LogLine
 func (ll LogLine) Length() int {
 	return len(ll.line)
 }
