f66375d9c03b6fc95f59e088555963861a0b082d
Edward Muller <edwardam@interlix.com>
2015-02-19 17:59:05 -0800

    Even though it doesn't work, test it.
    This is mostly to ensure that the composition is correct and to avoid a
    race condition again.

--- kinesis_formatter_test.go
+++ kinesis_formatter_test.go
@@ -1,6 +1,7 @@
 package shuttle
 
 import (
+	"compress/gzip"
 	"io/ioutil"
 	"testing"
 )
@@ -19,19 +20,45 @@ func TestKinesisFormatter(t *testing.T) {
 	t.Logf("%q", string(d))
 }
 
+func TestKinesisFormatterRequest(t *testing.T) {
+	config.LogsURL = "https://key:secret@kinesis.us-east-1.amazonaws.com/Stream"
+	b := NewBatch(1)
+	b.Add(LogLineOne)
+	b.Add(LogLineTwo)
+	kf := NewKinesisFormatter(b, noErrData, &config)
+	r, err := kf.Request()
+	if err != nil {
+		t.Fatalf("Unexpected error calling Request: ", err)
+	}
+
+	// Read the body of the request
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		t.Fatalf("Error reading everything from the request: %q", err)
+	}
+
+	t.Logf("%q", string(d))
+}
+
 func TestKinesisGzip(t *testing.T) {
 	config.LogsURL = "https://key:secret@foo/Stream"
 	b := NewBatch(1)
 	b.Add(LogLineOne)
 	kf := NewKinesisFormatter(b, noErrData, &config)
 
-	// Expecting a panic here?
 	gf := NewGzipFormatter(kf)
 
-	d, err := ioutil.ReadAll(gf)
+	// decompress the bytes and verify the message
+	gunzipper, err := gzip.NewReader(gf)
 	if err != nil {
-		t.Fatal("Didn't expect an error reading all data from the gzip formatter, but got: ", err)
+		t.Fatal("Error making a reader: ", err)
 	}
 
-	t.Log("Data: ", d)
+	// read the uncompressed bytes
+	uncompressed, err := ioutil.ReadAll(gunzipper)
+	if err != nil {
+		t.Fatal("Errors reading the compressed bytes: ", err)
+	}
+
+	t.Log("Data: ", string(uncompressed))
 }
