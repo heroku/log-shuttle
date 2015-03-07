21bfa0abdeda0fb1d88762445d13681d2517deb2
Edward Muller <edwardam@interlix.com>
2015-03-06 16:32:44 -0800

    First pass at s3 outlet
    This is pretty crappy and is here right now just to experiment

---
+++ s3_outlet.go
@@ -0,0 +1,39 @@
+package shuttle
+
+import (
+	"io"
+
+	"github.com/kr/s3/s3util"
+)
+
+type S3Outlet struct {
+	config           Config
+	inbox            <-chan Batch
+	newFormatterFunc NewHTTPFormatterFunc
+}
+
+func NewS3Outlet(s *Shuttle) Outlet {
+	return &S3Outlet{
+		config:           s.config,
+		inbox:            s.Batches,
+		newFormatterFunc: s.config.FormatterFunc,
+	}
+}
+
+func (s3o *S3Outlet) Outlet() {
+	//TODO: apply format to url to make unique objects over time
+	w, err := s3util.Create(s3o.config.LogsURL, nil, nil)
+	//TODO: do something better with errors
+	if err != nil {
+		panic(err)
+	}
+	for batch := range s3o.inbox {
+		f := s3o.newFormatterFunc(batch, nil, &s3o.config)
+		_, err = io.Copy(w, f)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+	}
+
+	w.Close()
+}
