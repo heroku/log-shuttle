aa0a414c793328a71132f7ffccfb14bd10ec4f17
Edward Muller <edwardam@interlix.com>
2015-03-30 20:06:28 -0700

    Don't use ReadAll.
    I know better. Not sure why I didn't think I could do this earlier.
    :-(

--- kinesis_types.go
+++ kinesis_types.go
@@ -3,7 +3,6 @@ package shuttle
 import (
 	"encoding/base64"
 	"io"
-	"io/ioutil"
 )
 
 // KinesisRecord is used to marshal LoglexLineFormatters to Kinesis Records for
@@ -14,17 +13,9 @@ type KinesisRecord struct {
 
 // WriteTo writes the LogplexLineFormatter to the provided writer
 // in Kinesis' PutRecordsFormat. Conforms to the WriterTo interface.
-//
-// Since the data should be relatively small just read the LogplexLineFormatter
-// into ram making a bunch of temporary garbage.  AFAICT it's just not worth it
-// to try and string these together with io.Pipes and the like. :-(
 func (r KinesisRecord) WriteTo(w io.Writer) (n int64, err error) {
 	var t int
-
-	b, err := ioutil.ReadAll(r.llf)
-	if err != nil {
-		return
-	}
+	var t64 int64
 
 	t, err = w.Write([]byte(`{"PartitionKey":"` + r.llf.AppName() + `","Data":"`))
 	n += int64(t)
@@ -34,12 +25,15 @@ func (r KinesisRecord) WriteTo(w io.Writer) (n int64, err error) {
 
 	e := base64.NewEncoder(base64.StdEncoding, w)
 
-	t, err = e.Write(b)
-	n += int64(t)
+	t64, err = io.Copy(e, r.llf)
+	n += int64(base64.StdEncoding.EncodedLen(int(t64)))
 	if err != nil {
 		return
 	}
-	e.Close()
+
+	if err = e.Close(); err != nil {
+		return
+	}
 
 	t, err = w.Write([]byte(`"}`))
 	n += int64(t)
