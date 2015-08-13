232cb983f1a88351cca8efdd788a05d19d754e8f
Edward Muller <edwardam@interlix.com>
2015-08-12 19:33:53 -0700

    Work with rc and beta releases of Go

--- shuttle_test.go
+++ shuttle_test.go
@@ -239,7 +239,7 @@ func TestUserAgentHeader(t *testing.T) {
 		t.Fatalf("Header User-Agent not found in response")
 	}
 
-	uaPattern := regexp.MustCompile(`^^log-shuttle/[0-9a-z-\.]+ \(go\d+(\.\d+){0,2}; \w+; \w+; \w+\)$`)
+	uaPattern := regexp.MustCompile(`^^log-shuttle/[0-9a-z-\.]+ \(go\d+(\.\d+){0,2}((beta|rc)\d)?; \w+; \w+; \w+\)$`)
 	if !uaPattern.MatchString(uaHeader[0]) {
 		t.Fatalf("Header User-Agent doesn't match expected pattern. Actual: %s\n", uaHeader[0])
 	}
