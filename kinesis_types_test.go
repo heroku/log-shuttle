03d614ab78cb54b8821b11467e5f40d852081526
Nathan Smith <nathansmith22@gmail.com>
2015-10-30 16:04:46 -0400

    [kinesis-sharding] Add test to ensure sharding works from config.

--- kinesis_types_test.go
+++ kinesis_types_test.go
@@ -44,3 +44,58 @@ func TestKinesisRecord_MarshalJSONToWriter(t *testing.T) {
 		t.Fatal("Expected Data to be equal to reading all the formatted bytes of the log line, but wasn't.")
 	}
 }
+
+func TestKinesisRecordSharding(t *testing.T) {
+	config := newTestConfig()
+	config.KinesisShards = 2
+	llf := NewLogplexLineFormatter(LogLineOne, &config)
+	llf2 := NewLogplexLineFormatter(LogLineTwo, &config)
+	b := new(bytes.Buffer)
+	r := KinesisRecord{llf}
+	r2 := KinesisRecord{llf2}
+
+	_, err := r.WriteTo(b)
+	if err != nil {
+		t.Fatal("Unexpected error calling MarshalJSONToWriter: ", err)
+	}
+
+	tr := struct {
+		Data         []byte
+		PartitionKey string
+	}{}
+
+	t.Logf("%+q\n", b.Bytes())
+
+	if err := json.Unmarshal(b.Bytes(), &tr); err != nil {
+		t.Fatal("Unexpected error unmashalling KinesisRecord: ", err)
+	}
+
+	t.Logf("%+q\n", tr)
+
+	if tr.PartitionKey != "shuttle1" {
+		t.Fatal("Expected PartitonKey to not be empty, but was.")
+	}
+
+	b = new(bytes.Buffer)
+	_, err = r2.WriteTo(b)
+	if err != nil {
+		t.Fatal("Unexpected error calling MarshalJSONToWriter: ", err)
+	}
+
+	tr = struct {
+		Data         []byte
+		PartitionKey string
+	}{}
+
+	t.Logf("%+q\n", b.Bytes())
+
+	if err := json.Unmarshal(b.Bytes(), &tr); err != nil {
+		t.Fatal("Unexpected error unmashalling KinesisRecord: ", err)
+	}
+
+	t.Logf("%+q\n", tr)
+
+	if tr.PartitionKey != "shuttle2" {
+		t.Fatal("Expected PartitonKey to not be empty, but was.")
+	}
+}
