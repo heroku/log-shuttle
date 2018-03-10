fdf2635419fdea3bd4a9eb21414ddb4b21e8b226
Edward Muller <emuller@salesforce.com>
2018-03-09 17:53:43 -0800

    Move License to separate file and add headers to files.
    This is for Salesforce OSS process.

--- cmd/log-shuttle/metrics_reporter.go
+++ cmd/log-shuttle/metrics_reporter.go
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018 Salesforce
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root
+ *   or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 package main
 
 import (
