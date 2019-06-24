7fafc04212cadbea47c9a47d2de9cf455244b84b
Jillian Tullo <jtullo@heroku.com>
2019-06-24 09:19:58 -0400

    cleanup metrics emitter and add in example to Emit coment

--- cmd/log-shuttle/main.go
+++ cmd/log-shuttle/main.go
@@ -258,8 +258,8 @@ func main() {
 	s.LoadReader(os.Stdin)
 
 	s.Launch()
-	metricsReporter := shuttle.NewMetricsReporter(s.MetricsRegistry, config.StatsSource, config.StatsInterval, s.Logger)
-	go metricsReporter.Emit()
+	metricsReporter := shuttle.NewMetricsReporter(s.MetricsRegistry, config.StatsSource, s.Logger)
+	go metricsReporter.Emit(config.StatsInterval)
 
 	// blocks until the readers all exit
 	s.WaitForReadersToFinish()
--- metrics_reporter.go
+++ metrics_reporter.go
@@ -25,15 +25,14 @@ func sec(t float64) string {
 type MetricsReporter struct {
 	registry metrics.Registry
 	source   string
-	duration time.Duration
 	logger   *log.Logger
 	lastCounts map[string]int64
 	doneCh chan struct{}
 }
 
 // NewMetricsReporter returns a properly constructed MetricsReporter
-func NewMetricsReporter(r metrics.Registry, source string, d time.Duration, l *log.Logger) *MetricsReporter {
-	return &MetricsReporter{registry: r, source: source, duration: d, logger: l, lastCounts: make(map[string]int64), doneCh: make(chan struct{})}
+func NewMetricsReporter(r metrics.Registry, source string, l *log.Logger) *MetricsReporter {
+	return &MetricsReporter{registry: r, source: source, logger: l, lastCounts: make(map[string]int64), doneCh: make(chan struct{})}
 }
 
 func (e MetricsReporter) countDifference(ctx slog.Context, name string, c int64) {
@@ -46,12 +45,26 @@ func (e MetricsReporter) countDifference(ctx slog.Context, name string, c int64)
 // Emit emits log-shuttle metrics in logfmt compatible formats every d
 // duration using the MetricsReporter logger. source is added to the line as
 // log_shuttle_stats_source if not empty. It waits for a stop signal
-// and will stop emitting metrics when received.
-func (e MetricsReporter) Emit() {
-	if e.duration == 0 {
+// and will stop emitting metrics when received. Example output:
+// space=<space-id> instance=<instance-id> runc-shuttle2019/06/10 12:48:25 batch.fill.count=0
+// batch.fill.max=0.000000 batch.fill.mean=0.000000 batch.fill.min=0.000000
+// batch.fill.p75=0.000000 batch.fill.p95=0.000000 batch.fill.p99=0.000000 batch.fill.rate.15min=0.000
+// batch.fill.rate.1min=0.000 batch.fill.rate.5min=0.000 batch.fill.rate.mean=0.000
+// batch.fill.stddev=0.000000 jw-debug-metrics=true lines.batched.count=0 lines.dropped.count=0
+// lines.read.count=0 msg.lost.count=0 outlet.inbox.length=0 outlet.post.failure.count=0
+// outlet.post.failure.max=0.000000 outlet.post.failure.mean=0.000000 outlet.post.failure.min=0.000000
+// outlet.post.failure.p75=0.000000 outlet.post.failure.p95=0.000000 outlet.post.failure.p99=0.000000
+// outlet.post.failure.rate.15min=0.000 outlet.post.failure.rate.1min=0.000 outlet.post.failure.rate.5min=0.000
+// outlet.post.failure.rate.mean=0.000 outlet.post.failure.stddev=0.000000 outlet.post.success.count=0
+// outlet.post.success.max=0.000000 outlet.post.success.mean=0.000000 outlet.post.success.min=0.000000
+// outlet.post.success.p75=0.000000 outlet.post.success.p95=0.000000 outlet.post.success.p99=0.000000
+// outlet.post.success.rate.15min=0.000 outlet.post.success.rate.1min=0.000 outlet.post.success.rate.5min=0.000
+// outlet.post.success.rate.mean=0.000 outlet.post.success.stddev=0.000000
+func (e MetricsReporter) Emit(d time.Duration) {
+	if d == 0 {
 		return
 	}
-	ticker := time.NewTicker(e.duration)
+	ticker := time.NewTicker(d)
 	defer ticker.Stop()
 
 	for {
